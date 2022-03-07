3bf1237e4f9e0d7d18a4eb3e9683d9b54e409d6c
mwdomino <mdominey@equinix.com>
2022-03-07 17:48:15 -0500

    Add setup.py

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(
+  name='keep-presence',
+  version='1.0`5',
+  install_requires=install_requires,
+  scripts=[
+    'keep-presence.py',
+  ]
+)
